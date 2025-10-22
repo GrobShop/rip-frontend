4c2b36b17e3370dc9bf45ed28b0deab6aeb64d03
CandyPopsWorld <bondarevzh@gmail.com>
2025-10-22 11:34:19 +0300

    Add admin-login service

---
+++ libs/shared-components/src/lib/data/api/api.routes.ts
@@ -0,0 +1,60 @@
+import {ApiSettings} from "./api.settings";
+
+export const ApiRoutes = {
+  ADMIN: {
+    LOGIN: {
+      LOGIN: ApiSettings.apiUrl + '/auth/login'
+    }
+  }
+  // REGISTER: {
+  //   SEND_EMAIL: ApiSettings.apiUrl + '/register/email',
+  //   VERIFY_CODE: ApiSettings.apiUrl + '/register/verify-code',
+  //   SET_PASSWORD: ApiSettings.apiUrl + '/register/set-password'
+  // },
+  // LOGIN: {
+  //   LOGIN: ApiSettings.apiUrl + '/login'
+  // },
+  // REFRESH_TOKEN: {
+  //   REFRESH: ApiSettings.apiUrl + '/refresh-token'
+  // },
+  // CATEGORY: {
+  //   CREATE_CATEGORY: ApiSettings.apiUrl + '/categories',
+  //   GET_ALL_CATEGORY_USER: ApiSettings.apiUrl + '/categories',
+  //   GET_CATEGORY_BY_ID: (categoryId: string) => ApiSettings.apiUrl + `/categories/${categoryId}`,
+  //   UPDATE_CATEGORY: (categoryId: string) => ApiSettings.apiUrl + `/categories/${categoryId}`,
+  //   DELETE_CATEGORY: (categoryId: string) => ApiSettings.apiUrl + `/categories/${categoryId}`,
+  // },
+  // PASSWORD: {
+  //   GENERATE_IV: ApiSettings.apiUrl + '/passwords/generate-iv',
+  //   CREATE_PASSWORD: ApiSettings.apiUrl + '/passwords',
+  //   GET_ALL_PASSWORDS: ApiSettings.apiUrl + '/passwords',
+  //   GET_PASSWORD_BY_ID: (id: string) => ApiSettings.apiUrl + `/passwords/${id}`,
+  //   UPDATE_PASSWORD: (id: string) => ApiSettings.apiUrl + `/passwords/${id}`,
+  //   DELETE_PASSWORD: (id: string) => ApiSettings.apiUrl + `/passwords/${id}`,
+  // },
+  // USER: {
+  //   GET_PROFILE: ApiSettings.apiUrl + '/user',
+  //   DELETE_PROFILE: ApiSettings.apiUrl + '/user',
+  //   DELETE_DATA: ApiSettings.apiUrl + '/passwords/user/data'
+  // },
+  // SETTINGS: {
+  //   GET_SETTINGS: ApiSettings.apiUrl + '/settings',
+  //   UPDATE_SETTINGS: ApiSettings.apiUrl + '/settings',
+  //   PATCH_SETTINGS: ApiSettings.apiUrl + '/settings'
+  // },
+  // HINT: {
+  //   REQUEST_HINT: ApiSettings.apiUrl + '/request-hint',
+  //   GET_HINT: ApiSettings.apiUrl + '/get-hint'
+  // },
+  // TWO_FA: {
+  //   ENABLE_2FA: ApiSettings.apiUrl + '/auth/enable-2fa',
+  //   CONFIRM_ENABLE_2FA: ApiSettings.apiUrl + '/auth/confirm-enable-2fa',
+  //   DISABLE_2FA: ApiSettings.apiUrl + '/auth/disable-2fa',
+  //   CONFIRM_DISABLE_2FA: ApiSettings.apiUrl + '/auth/confirm-disable-2fa',
+  //   VERIFY_2FA: ApiSettings.apiUrl + '/verify-2fa'
+  // },
+  // BACKUPS: {
+  //   PASSWORDS_BACKUPS: ApiSettings.apiUrl + '/passwords/backup',
+  //   PASSWORDS_RESTORE: ApiSettings.apiUrl + '/passwords/restore'
+  // }
+};
---
+++ libs/shared-components/src/lib/data/vault/store.keys.ts
@@ -0,0 +1,7 @@
+export const enum StoreKeys{
+  ACCESS_TOKEN = 'accessToken',
+  REFRESH_TOKEN = 'refreshToken',
+  USER_ID = 'userId',
+  USER_EMAIL = 'userEmail',
+  USER_ROLE = 'userRole',
+}
