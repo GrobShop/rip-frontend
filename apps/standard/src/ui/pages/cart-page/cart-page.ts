4ac76757414b1a3664a92d2da6ab16198915dc9f
CandyPopsWorld <bondarevzh@gmail.com>
2025-10-31 01:44:37 +0300

    Update cart-page

--- apps/standard/src/ui/pages/cart-page/cart-page.ts
+++ apps/standard/src/ui/pages/cart-page/cart-page.ts
@@ -7,9 +7,15 @@ import {ButtonComponent} from "../../../../../../libs/shared-components/src/lib/
 import {
   ProductCardComponent
 } from "../../../../../../libs/shared-components/src/lib/components/cards/product/product-card/product-card-component";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {InputComponent} from "../../../../../../libs/shared-components/src/lib/components/input/input-component";
 import { Router, RouterModule } from '@angular/router';
+import {Product} from "../../../../../../libs/shared-components/src/lib/interfaces/product.interface";
+import {WishlistItemLocal} from "../../../services/routes/whislist/whislist-local.service";
+import {CartItem, CartService} from "../../../services/routes/cart/cart.service";
+import {CartItemLocal, CartLocalService} from "../../../services/routes/cart/cart-local.service";
+import {ProductService} from "../../../services/routes/product/product.service";
+import {ProductLocalService} from "../../../services/routes/product/product-local.service";
 
 @Component({
   selector: 'app-cart-page',
@@ -19,19 +25,44 @@ import { Router, RouterModule } from '@angular/router';
     ButtonComponent,
     ProductCardComponent,
     NgIf,
-    InputComponent
+    InputComponent,
+    NgForOf
   ],
   templateUrl: './cart-page.html',
   styleUrl: './cart-page.css',
   standalone: true
 })
 export class CartPage {
-  totalAmount: number = 5500;
-  totalCount: number = 5;
+  totalAmount: number = 0;
+  totalCount: number = 0;
   isOrder: boolean = false;
 
-  constructor(private router: Router) {}
+  products: Product[] = [];
+  cartItems: CartItemLocal[] = [];
+  productsIds: string[] = [];
 
+  shipMethod: string = '';
+  paymentMethod: string = '';
+
+  constructor(private router: Router, private cartService: CartService, private cartLocalService: CartLocalService, private productService: ProductService, private productLocalService: ProductLocalService) {}
+
+  async ngOnInit(){
+    await this.cartLocalService.syncCart();
+    this.cartItems = this.cartLocalService.getItems();
+
+    if(this.cartItems.length == 0){return;}
+
+    this.cartItems.forEach((item) => {
+      this.productsIds.push(item.product_id);
+    })
+
+    for (const id of this.productsIds) {
+      const product = await this.productLocalService.getProductById(id);
+      this.totalAmount += Number(product.price);
+      this.products.push(product);
+    }
+    this.totalCount = this.cartLocalService.getTotalItems();
+  }
 
   onOrder(){
     this.isOrder = true;
@@ -43,5 +74,17 @@ export class CartPage {
 
   async goHomepage(){
     await this.router.navigate(['/categories']);
+  }
+
+  onChangeShipMethod($event: string) {
+    this.shipMethod = $event;
+  }
+
+  onChangePaymentMethod($event: string) {
+    this.paymentMethod = $event;
+  }
+
+  onSubmitCartRequest() {
+
   }
 }
